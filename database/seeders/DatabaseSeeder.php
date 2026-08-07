8246349568e88f05fcff1c5dfefdf9f3b3bafd18
Callmhetcp <dannywiz480@gmail.com>
2026-08-07 09:00:10 +0100

    Test 4 seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
                 AdminUserSeeder::class,
                 DemoCategorySeeder::class,
                 DemoSupplierSeeder::class,
+                DemoMedicineSeeder::class,
             ]);
         }
 }
