82b1ec83f2a22e9dc195f570b9ba32c9459362c1
Callmhetcp <dannywiz480@gmail.com>
2026-08-07 09:29:39 +0100

    Customer seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
                 DemoCategorySeeder::class,
                 DemoSupplierSeeder::class,
                 DemoMedicineSeeder::class,
+                DemoCustomerSeeder::class,
             ]);
         }
 }
